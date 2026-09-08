43dac5884729e937ecd0163b1b630b721decb0ef
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-08 21:10:59 +0500

    Update AccountClassTest.java

--- AccountClassTest.java
+++ AccountClassTest.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class AccountClassTest {
     
-    public static void main(String[]args)
-    {
+    public static void main(String[]args){
+        
         Scanner myObj = new Scanner(System.in);
         
         System.out.println("Enter account number");
@@ -12,21 +12,23 @@ public class AccountClassTest {
         System.out.println("Enter balance ");
         double userbalance= myObj.nextDouble();  
         
-        System.out.println("User Account= is: " + userAcc); 
+        System.out.println("User Account= is: " + userAcc);
+        
         System.out.println("User balce= is: " + userbalance); 
         
         AccountClass a1=new AccountClass(userAcc,userbalance);
         
-        
         System.out.println("---------User Account= is: " + a1.getaccountnumber()); 
+        
         System.out.println("----------User balce= is: " + a1.getbalance());
         
         a1.setAccountnumber(150);
+        
         a1.setBalance(500000);
         
         System.out.println("---------User Account= is: " + a1.getaccountnumber()); 
+        
         System.out.println("----------User balce= is: " + a1.getbalance());
         
-  
     }
 }
